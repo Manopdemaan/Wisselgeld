e6db54f22c3f6d52dc166668d60f6b66b671ba11
Manopdemaan <112510226+Manopdemaan@users.noreply.github.com>
2023-10-20 11:50:23 +0200

    wissel

---
+++ wisselgeld.php
@@ -0,0 +1,23 @@
+<?php
+
+if (count($argv) != 2) {
+    echo "Gebruik: php wisselgeld.php <bedrag>" . PHP_EOL;
+    exit(1);
+}
+
+$bedrag = intval($argv[1]);
+
+if ($bedrag < 0) {
+    echo "Ongeldig bedrag." . PHP_EOL;
+    exit(1);
+}
+
+$wisselgeld = $bedrag;
+
+if ($wisselgeld == 0) {
+    echo "geen wisselgeld" . PHP_EOL;
+} else {
+    echo "$wisselgeld x 1 euro" . PHP_EOL;
+}
+
+?>
