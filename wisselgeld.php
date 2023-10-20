77500c4af008fbbc43ee900f0569a4f447497292
Manopdemaan <112510226+Manopdemaan@users.noreply.github.com>
2023-10-20 12:30:17 +0200

    ws

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,7 +1,7 @@
 <?php
 
 if (count($argv) != 2) {
-    echo "Gebruik: php wisselgeld.php <bedrag>" . PHP_EOL;
+    echo "geen wissegeld" . PHP_EOL;
     exit(1);
 }
 
@@ -12,20 +12,20 @@ if ($bedrag < 0) {
     exit(1);
 }
 
-$biljettenVan10 = floor($bedrag / 10);
-$bedrag = $bedrag - ($biljettenVan10 * 10);
+$muntenVan5 = floor($bedrag / 5);
+$bedrag = $bedrag - ($muntenVan5 * 5);
 
-$biljettenVan5 = floor($bedrag / 5);
-$bedrag = $bedrag - ($biljettenVan5 * 5);
+$muntenVan2 = floor($bedrag / 2);
+$bedrag = $bedrag - ($muntenVan2 * 2);
 
 $muntenVan1 = $bedrag;
 
-if ($biljettenVan10 > 0) {
-    echo "$biljettenVan10 x 10 euro" . PHP_EOL;
+if ($muntenVan5 > 0) {
+    echo "$muntenVan5 x 5 euro" . PHP_EOL;
 }
 
-if ($biljettenVan5 > 0) {
-    echo "$biljettenVan5 x 5 euro" . PHP_EOL;
+if ($muntenVan2 > 0) {
+    echo "$muntenVan2 x 2 euro" . PHP_EOL;
 }
 
 if ($muntenVan1 > 0) {
