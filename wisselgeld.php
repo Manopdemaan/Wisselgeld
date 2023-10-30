d6663268114ce844847c18243ae48ca1ef8537bc
Manopdemaan <112510226+Manopdemaan@users.noreply.github.com>
2023-10-30 11:39:39 +0100

    wi

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,43 +1,77 @@
 <?php
 
-class NoAmountException extends Exception {}
-class NegativeAmountException extends Exception {}
-class InvalidAmountException extends Exception {}
-
-function validateInput($input) {
-    if (empty($input)) {
-        throw new NoAmountException("Geen bedrag meegegeven.");
+function validateInput($input)
+{
+    if (empty($input) || !is_numeric($input)) {
+        throw new Exception("Input moet een valide getal zijn");
     }
 
-    if (!is_numeric($input)) {
-        throw new InvalidAmountException("Geen wisselgeld");
-    }
+    $amount = floatval($input);
 
-    $amount = floatval(str_replace(',', '.', $input));
-
-    if ($amount < 0) {
-        throw new NegativeAmountException("Negatief bedrag meegegeven.");
-    }
-
-    if ($amount == 0) {
-        throw new InvalidAmountException("Ongeldig bedrag meegegeven.");
+    if ($amount <= 0) {
+        throw new Exception("Input moet een positief getal zijn");
     }
 
     return $amount;
 }
 
-if (isset($argv) && count($argv) > 1) {
-    try {
-        $inputAmount = validateInput($argv[1]);
-    } catch (NoAmountException | NegativeAmountException | InvalidAmountException $e) {
-        echo $e->getMessage() . PHP_EOL;
-    } catch (TypeError $e) {
-        echo "TypeError: " . $e->getMessage() . PHP_EOL;
-    } catch (Exception $e) {
-        echo $e->getMessage() . PHP_EOL;
+const MONEY_UNITS = [5000, 10000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+const CENT_UNITS = [50, 20, 10, 5, 2, 1];
+
+function calculateChangeInMoneys($changeInCents)
+{
+    foreach (MONEY_UNITS as $moneyUnit) {
+        if ($changeInCents >= $moneyUnit) {
+            $numberOfUnits = floor($changeInCents / $moneyUnit);
+            $changeInCents -= $numberOfUnits * $moneyUnit;
+
+            if ($moneyUnit >= 100) {
+                $unitName = ($moneyUnit / 100) . " euro";
+            } else if ($moneyUnit == 50) {
+                $unitName = "50 cent";
+            } else {
+                $unitName = $moneyUnit . " cent";
+            }
+
+            echo $numberOfUnits . " x " . $unitName . PHP_EOL;
+        }
     }
-} else {
-    echo "Geen wisselgeld" . PHP_EOL;
-    exit(1);
+
+    return $changeInCents;
 }
-?>
+
+function calculateChangeInCents($changeInCents)
+{
+    $changeInCents = round($changeInCents / 5) * 5;
+
+    foreach (CENT_UNITS as $centUnit) {
+        if ($changeInCents >= $centUnit) {
+            $numberOfUnits = floor($changeInCents / $centUnit);
+            $changeInCents -= $numberOfUnits * $centUnit;
+            echo $numberOfUnits . " x " . $centUnit . " cent" . PHP_EOL;
+        }
+    }
+}
+
+function calculateChange($amount)
+{
+    $amount = round($amount * 20) / 20;
+    $amountInCents = $amount * 100;
+    $changeInCents = $amountInCents;
+
+    $changeInCents = calculateChangeInMoneys($changeInCents);
+    calculateChangeInCents($changeInCents);
+}
+
+try {
+    if ($argc < 2) {
+        throw new Exception("geen wisselgeld");
+    }
+
+    $amount = validateInput($argv[1]);
+
+    calculateChange($amount);
+} catch (Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+    exit;
+}
