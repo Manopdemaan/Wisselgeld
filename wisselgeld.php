1645b68c71eb6b49262a67e6305b6a4d6f29fc18
Manopdemaan <112510226+Manopdemaan@users.noreply.github.com>
2023-10-30 09:13:20 +0100

    wissel

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,18 +1,18 @@
 <?php
 
 if (count($argv) != 2) {
-    echo "Gebruik een getal" . PHP_EOL;
+    echo "Geen wisselgeld" . PHP_EOL;
     exit(1);
 }
 
-$bedrag = floatval(str_replace(',', '.', $argv[1])); 
+$bedrag = floatval(str_replace(',', '.', $argv[1]));
 
 if ($bedrag < 0) {
     echo "Ongeldig bedrag bro." . PHP_EOL;
     exit(1);
 }
 
-define('MONEY_UNITS', [10.0, 5.0, 2.0, 1.0, 0.5, 0.2, 0.1, 0.05]);
+define('MONEY_UNITS', [100.0, 50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.5, 0.2, 0.1, 0.05]);
 
 $restbedrag = $bedrag;
 
@@ -21,15 +21,13 @@ foreach (MONEY_UNITS as $geldeenheid) {
         $aantalKeerGeldEenheidInRestBedrag = floor($restbedrag / $geldeenheid);
         if ($aantalKeerGeldEenheidInRestBedrag > 0) {
             if ($geldeenheid >= 1) {
-                if ($geldeenheid == 1.0) {
-                    echo "1 x 1 euro" . PHP_EOL;
-                } else {
-                    echo "$aantalKeerGeldEenheidInRestBedrag x " . intval($geldeenheid) . " euro" . PHP_EOL;
-                }
+                echo "$aantalKeerGeldEenheidInRestBedrag x " . intval($geldeenheid) . " euro" . PHP_EOL;
             } else {
-                echo "$aantalKeerGeldEenheidInRestBedrag x " . intval($geldeenheid * 100) . " cent" . PHP_EOL;
+                if ($geldeenheid < 1 && $geldeenheid >= 0.01) {
+                    echo "$aantalKeerGeldEenheidInRestBedrag x " . intval($geldeenheid * 100) . " cent" . PHP_EOL;
+                }
             }
-            $restbedrag = round(fmod($restbedrag, $geldeenheid), 2); 
+            $restbedrag = round(fmod($restbedrag, $geldeenheid), 2);
         }
     }
 }
