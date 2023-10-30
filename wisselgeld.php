95554eb877c126d552eef1276e9613559fb654ef
Manopdemaan <112510226+Manopdemaan@users.noreply.github.com>
2023-10-30 09:42:56 +0100

    wis

--- wisselgeld.php
+++ wisselgeld.php
@@ -1,44 +1,58 @@
 <?php
 
+function validateInput($input) {
+    return floatval(str_replace(',', '.', $input));
+}
+
+function roundToNearestFiveCents($amount) {
+    return round($amount * 20) / 20;
+}
+
+function calculateChange($amount) {
+    $change = [];
+    define('MONEY_UNITS', [50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.5, 0.2, 0.1, 0.05]);
+
+    foreach (MONEY_UNITS as $unit) {
+        $numberOfUnits = floor($amount / $unit);
+        if ($numberOfUnits > 0) {
+            if ($unit >= 1) {
+                if ($unit == 1.0) {
+                    $change[] = "1 x 1 euro";
+                } else {
+                    $change[] = "$numberOfUnits x " . intval($unit) . " euro";
+                }
+            } else {
+                if ($unit >= 0.01) {
+                    if ($unit == 0.05) {
+                        $change[] = "$numberOfUnits x 5 cent";
+                    } else {
+                        $change[] = "$numberOfUnits x " . intval($unit * 100) . " cent";
+                    }
+                }
+            }
+            $amount = round(fmod($amount, $unit), 2);
+        }
+    }
+
+    return $change;
+}
+
 if (count($argv) != 2) {
     echo "Geen wisselgeld" . PHP_EOL;
     exit(1);
 }
 
-$bedrag = floatval(str_replace(',', '.', $argv[1]));
-
-if ($bedrag < 0) {
+$inputAmount = validateInput($argv[1]);
+if ($inputAmount < 0) {
     echo "Ongeldig bedrag bro." . PHP_EOL;
     exit(1);
 }
 
-$roundedBedrag = round($bedrag * 20) / 20;
+$roundedAmount = roundToNearestFiveCents($inputAmount);
+$change = calculateChange($roundedAmount);
 
-define('MONEY_UNITS', [50.0, 20.0, 10.0, 5.0, 2.0, 1.0, 0.5, 0.2, 0.1, 0.05]);
-
-$restbedrag = $roundedBedrag;
-
-foreach (MONEY_UNITS as $geldeenheid) {
-    if ($restbedrag >= $geldeenheid) {
-        $aantalKeerGeldEenheidInRestBedrag = floor($restbedrag / $geldeenheid);
-        if ($aantalKeerGeldEenheidInRestBedrag > 0) {
-            if ($geldeenheid >= 1) {
-                if ($geldeenheid == 1.0) {
-                    echo "1 x 1 euro" . PHP_EOL;
-                } else {
-                    echo "$aantalKeerGeldEenheidInRestBedrag x " . intval($geldeenheid) . " euro" . PHP_EOL;
-                }
-            } else {
-                if ($geldeenheid >= 0.01) {
-                    if ($geldeenheid == 0.05) {
-                        echo "$aantalKeerGeldEenheidInRestBedrag x 5 cent" . PHP_EOL;
-                    } else {
-                        echo "$aantalKeerGeldEenheidInRestBedrag x " . intval($geldeenheid * 100) . " cent" . PHP_EOL;
-                    }
-                }
-            }
-            $restbedrag = round(fmod($restbedrag, $geldeenheid), 2);
-        }
-    }
+foreach ($change as $changeItem) {
+    echo $changeItem . PHP_EOL;
 }
+
 ?>
